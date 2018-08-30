6edd6a99aec596f2f4ad3372057eb8bad2340c72
Nate Druss <nate@nsideas.com>
2018-08-30 16:18:38 -0400

    Cleared out old styles

--- src/js/fairport.js
+++
@@ -1,6 +0,0 @@
-
-// @codekit-append 'partials/global.js';
-// @codekit-append 'partials/hero-swiper.js';
-// @codekit-append 'partials/fam-circles.js';
-// @codekit-append 'partials/staff-list.js';
-// @codekit-append 'partials/dropdown.js';
---
+++ src/js/luma.js
@@ -0,0 +1,2 @@
+
+// @codekit-prepend 'partials/global.js';
