cd79b261088a538fccbfee4c35230db2522f454f
Nate Druss <nate@nsideas.com>
2018-09-07 17:29:25 -0400

    Header top menu open close

--- src/js/partials/global.js
+++ src/js/partials/global.js
@@ -2,7 +2,7 @@
 // Media Queries
 var mq_small = window.matchMedia('(min-width: 480px)');
 var mq_medium = window.matchMedia('(min-width: 720px)');
-var mq_large = window.matchMedia('(min-width: 840px)');
+var mq_large = window.matchMedia('(min-width: 860px)');
 
 if (bowser.msie) {
   document.body.classList.add('ie');
@@ -11,9 +11,9 @@ if (bowser.msie) {
 $('#menu-toggle').click(function() {
   var $nav = $('#header-nav');
   if (!$nav.hasClass('open')) {
-    $nav.slideDown(300).addClass('open');
+    $nav.slideDown(300).addClass('open').removeClass('closed');
   } else {
-    $nav.slideUp(300).removeClass('open');
+    $nav.slideUp(300).removeClass('open').addClass('closed');
   }
 });
 
@@ -21,8 +21,17 @@ $('#menu-toggle').click(function() {
 $('#toggle-top-menu').click(function() {
   var $topMenu = $(this).prev('.header-top-container');
   if (!$topMenu.hasClass('open')) {
-    $topMenu.slideDown(300).addClass('open');
+    $topMenu.slideDown(300).addClass('open').removeClass('closed');
   } else {
-    $topMenu.slideUp(300).removeClass('open');
+    $topMenu.slideUp(300).removeClass('open').addClass('closed');
   }
 });
+
+function handleResize() {
+  if (mq_large.matches) {
+    $('#header-nav').attr('style', '');
+    $('.header-top-container').attr('style', '');
+  }
+}
+
+window.addEventListener('resize', throttle(handleResize, 200));
