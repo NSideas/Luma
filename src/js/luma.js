1803c4033471687c99f505689270c0f9bbd72c5d
Nate Druss <nate@nsideas.com>
2018-09-14 17:27:00 -0400

    Form overlay start

--- src/js/luma.js
+++ src/js/luma.js
@@ -3,6 +3,26 @@
 // @codekit-prepend 'partials/header.js';
 
 
+function openFormOverlay($form) {
+  $form.fadeIn(300);
+  $('body').addClass('no-scroll');
+}
+
+function closeFormOverlay($form) {
+  $form.fadeOut(300);
+  $('body').removeClass('no-scroll');
+}
+
+$('#refer-a-friend-btn').click(function(e) {
+  e.preventDefault();
+  openFormOverlay($('#refer-a-friend'));
+});
+
+$('.overlay-module .btn-close').click(function() {
+  var $overlay = $(this).closest('.overlay-module');
+  closeFormOverlay($overlay);
+});
+
 function globalResizeHandler() {
   headerResize();
 }
