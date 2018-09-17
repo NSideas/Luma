96f4a4ac474a55cae318dcf3070be42c4c34eefc
Nate Druss <nate@nsideas.com>
2018-09-17 17:11:11 -0400

    Form overlay styles and functions

--- src/js/luma.js
+++ src/js/luma.js
@@ -4,12 +4,15 @@
 
 
 function openFormOverlay($form) {
-  $form.fadeIn(300);
+  $form.fadeIn(200, function() {
+    $form.find('.overlay-module--inner-wrap').addClass('visible');
+  });
   $('body').addClass('no-scroll');
 }
 
 function closeFormOverlay($form) {
-  $form.fadeOut(300);
+  $form.find('.overlay-module--inner-wrap').removeClass('visible');
+  $form.fadeOut(200);
   $('body').removeClass('no-scroll');
 }
 
@@ -21,6 +24,13 @@ $('#refer-a-friend-btn').click(function(e) {
 $('.overlay-module .btn-close').click(function() {
   var $overlay = $(this).closest('.overlay-module');
   closeFormOverlay($overlay);
+});
+
+$('.overlay-module--outer-wrap').click(function(e) {
+  if (!e.target.closest('.overlay-module--inner-wrap')) {
+    var $overlay = $(this).closest('.overlay-module');
+    closeFormOverlay($overlay);
+  }
 });
 
 function globalResizeHandler() {
