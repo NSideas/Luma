d39f85d85aa07277ff92f3feb18b6bfacf8bdb3a
Nate Druss <nate@nsideas.com>
2018-09-18 14:52:50 -0400

    Team member page desktop styles

--- src/js/luma.js
+++ src/js/luma.js
@@ -37,14 +37,37 @@ $('.overlay-module--outer-wrap').click(function(e) {
 
 // Team
 
-$('.team-member-link').click(function(e) {
-  e.preventDefault();
-  $(this).parent().siblings().removeClass('active');
-  $(this).parent().toggleClass('active');
-});
 
-$('.team-member .btn-close').click(function() {
-  $(this).closest('.team-member').removeClass('active');
+function staffList() {
+  var containWidth = $('#staff-list').width();
+  var containLeft = $('#staff-list')[0].getBoundingClientRect().x;
+  $('.team-member-bio').each(function() {
+    $(this).width(containWidth);
+    var leftOffset = $(this)[0].getBoundingClientRect().x;
+    if (leftOffset !== containLeft) {
+      var difference = leftOffset - containLeft;
+      $(this).css('left', '-' + difference + 'px');
+    }
+  });
+}
+
+
+$(document).ready(function() {
+
+  if ($('#staff-list').length) {
+    staffList();
+  }
+
+  $('.team-member-link').click(function(e) {
+    e.preventDefault();
+    $(this).parent().siblings().removeClass('active');
+    $(this).parent().toggleClass('active');
+  });
+
+  $('.team-member .btn-close').click(function() {
+    $(this).closest('.team-member').removeClass('active');
+  });
+
 });
 
 
