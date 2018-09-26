e7ec69acbf24df619ee3040e8d14c985cdfe1504
Nate Druss <nate@nsideas.com>
2018-09-26 14:14:08 -0400

    Global scroll function

--- src/js/luma.js
+++ src/js/luma.js
@@ -13,3 +13,29 @@ function globalResizeHandler() {
 }
 
 window.addEventListener('resize', debounce(globalResizeHandler, 100));
+
+var header_fixed = false;
+
+function headerScroll() {
+  if (window.scrollY > 120 && !header_fixed) {
+    $('#header').addClass('fixed');
+    header_fixed = true;
+  } else if (window.scrollY <= 120) {
+    $('#header').removeClass('fixed');
+    header_fixed = false;
+  }
+}
+
+var timeout;
+
+function globalScrollHandler() {
+  console.log(window.scrollY);
+  clearTimeout(timeout);
+  headerScroll();
+  timeout = setTimeout(function() {
+    console.log('time out');
+    headerScroll();
+  }, 150);
+}
+
+window.addEventListener('scroll', throttle(globalScrollHandler, 100));
