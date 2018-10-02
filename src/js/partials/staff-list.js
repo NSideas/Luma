89454d1eb14af9cd35345bcf1cfebb51eace6b1a
Nate Druss <nate@nsideas.com>
2018-10-02 16:00:00 -0400

    Scroll to clicked team member

--- src/js/partials/staff-list.js
+++ src/js/partials/staff-list.js
@@ -8,6 +8,7 @@ function staffList() {
     $(this).css('left', '0');
     $(this).width(containWidth);
     var leftOffset = $(this)[0].getBoundingClientRect().x;
+
     if (leftOffset !== containLeft) {
       var difference = leftOffset - containLeft;
       $(this).css('left', '-' + difference + 'px');
@@ -21,8 +22,12 @@ if ($('#staff-list').length) {
 
 $('.team-member-link').click(function(e) {
   e.preventDefault();
-  $(this).parent().siblings().removeClass('active');
-  $(this).parent().toggleClass('active');
+  var $teamMember = $(this).parent();
+  $teamMember.siblings().removeClass('active');
+  $teamMember.toggleClass('active');
+  var amountToScroll = $teamMember.offset().top;
+  amountToScroll -= mq_small.matches ? 110 : 90;
+  $(document).scrollTop(amountToScroll);
 });
 
 $('.team-member .btn-close').click(function() {
