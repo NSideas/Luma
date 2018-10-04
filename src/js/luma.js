12d55594592bbe6af5d837542e9833bc4fb22923
Nate Druss <nate@nsideas.com>
2018-10-04 16:49:21 -0400

    Added scroll top button

--- src/js/luma.js
+++ src/js/luma.js
@@ -15,12 +15,27 @@ function globalResizeHandler() {
 
 window.addEventListener('resize', debounce(globalResizeHandler, 100));
 
+var top_button_visible = false;
+
+function toggleScrollTop(distance) {
+  if (window.scrollY > distance && !top_button_visible) {
+    $('#scroll-top-container').fadeIn(300);
+    top_button_visible = true;
+  } else if (window.scrollY <= distance) {
+    $('#scroll-top-container').fadeOut(300);
+    top_button_visible = false;
+  }
+}
+
 
 var timeout;
 
 function globalScrollHandler() {
   clearTimeout(timeout);
   headerScroll();
+  if (mq_medium.matches) {
+    toggleScrollTop(window.innerHeight/2);
+  }
   timeout = setTimeout(function() {
     headerScroll();
   }, 150);
